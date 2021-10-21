08bd5008dd7339886e34484f56b799a17c9b0450
cwtan-delft <91547811+cwtan-delft@users.noreply.github.com>
2021-10-21 09:06:20 +0200

    Update assignment_B_TanCW.py
    Regiressions complete

--- assignment_B_TanCW.py
+++ assignment_B_TanCW.py
@@ -10,8 +10,59 @@ from assignment_B_model import calibration
 
 import numpy as np
 import pandas as pd
+import math
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
+from sklearn.model_selection import KFold
+
+#%% functions
+# function to calculated percentage bias
+def percent_bias(obs_array,sim_array):
+    '''
+    Arguments:np array of observed values, np array of simulated values \n
+    Returns: PBIAS value of simulated values
+    '''
+    if len(obs_array) != len(sim_array):
+        print("percentBias: !!! inputs do not have the same length")
+        return
+    else:    
+        pbias = 100*(sum(sim_array-obs_array)/sum(obs_array))
+    return pbias
+
+# function to calculate variations of mean square error (mse): mse, root mse (rmse), normalised rmse (nrmse)
+def nrmse(obs_array,sim_array):
+    '''
+    Arguments:np array of observed values, np array of simulated values \n
+    Returns: mean-square-error,root-mean-square-error,normalised-root-mean-square-error(std dev) \n
+    '''
+    if len(obs_array) != len(sim_array):
+        print("nrmse: !!! inputs do not have the same length")
+        return
+    else:
+
+        mse_val = np.sum((sim_array - obs_array)**2)/len(obs_array)
+        rmse_val = math.sqrt(mse_val)
+        nrmse_val = rmse_val/np.std(obs_array)
+    
+    return mse_val,rmse_val,nrmse_val
+
+def evaluation (obs_array, sim_array):
+    if len(obs_array) < 2:
+        r2_value = pbias_value = nrmse_value = math.nan
+        
+    else: 
+        r_value, p_value = pearsonr(obs_array, sim_array)
+        r2_value = r_value**2
+        
+        pbias_value = percent_bias(obs_array, sim_array)
+        
+        mse_value, rmse_value, nrmse_value = nrmse(obs_array, sim_array)
+    
+    return r2_value, pbias_value, nrmse_value
+
+def five_fold (time, value, folds):
+    kfold = KFold(folds)
+    for train, test in kfold.split(obs_array)
 #%% Import Data
 excel = pd.read_excel("Goal13.xlsx","data")
 
@@ -51,14 +102,21 @@ projection_year = 2030
 logistic_results = {}
 
 for country in excel_affected["GeoAreaName"].unique():
+    #for debug
+    print(country)
+    
+    #initialise values and dictionaries
     logistic_results[country] = {}
     x = excel_affected.loc[excel_affected.GeoAreaName == country, 'TimePeriod']
     y = excel_affected.loc[excel_affected.GeoAreaName == country, 'Value']
+    
+    #run logistic calibration
     start, K, x_peak, r = calibration(x,y)
-
+    
+    #run logistic function with calibration values
     lf_2030 = logistic(projection_year, start, K, x_peak, r)
-    logistic_results[country]['TimePeriod'] = x
-    logistic_results[country]['Value'] = y
+    logistic_results[country]['TimePeriod'] = x.tolist()
+    logistic_results[country]['Value'] = y.tolist()
     logistic_results[country]['Calibration'] = (start, K, x_peak, r)
     logistic_results[country]['2030 Logistic'] = lf_2030
     logistic_results[country]['Growth Rate'] = r
@@ -66,17 +124,15 @@ for country in excel_affected["GeoAreaName"].unique():
     #generating logistic model results for datapoints
     lf_list = [logistic(year, start, K, x_peak, r) for year in x]
     logistic_results[country]['Logistic Series'] = lf_list
+    
+    #performing Evaluation of regression
+    r2_value, pbias_value, nrmse_value = evaluation(y,lf_list)
+    
+    logistic_results[country]['R2'] = r2_value
+    logistic_results[country]['PBIAS'] = pbias_value
+    logistic_results[country]['Calibration'] = nrmse_value
+    
+    if math.isnan(r2_value):
+        print("->{} has {} datapoints, not enough for regression".format(country,len(lf_list)))
 
     
-    
-#performing Linear Regression
-print("\n*Regression")
-# calculating r2 with McFadden's R2
-r_squared_McFadden = 1 - (np.log()/np.log())
-# calculating r2 with Cox and Snell's R2
-# print('\n*PBIAS')
-# lr_pbias = percentBias(reg_year_temp[:,1],lr_array[:,1])
-# print('\n*NRMSE')
-# lr_nrmse = math.sqrt(mean_squared_error(reg_year_temp[:,1],lr_array[:,1]))/np.std(reg_year_temp[:,1])
-# lr_mse,lr_rmse,lr_nrmse = nrmse(reg_year_temp[:,1],lr_array[:,1])
-
